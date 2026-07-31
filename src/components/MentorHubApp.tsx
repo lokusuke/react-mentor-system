f8513a83b043c1ea94b215da9885a206fce68082
lokusuke <suzloku63@gmail.com>
2026-08-01 01:42:19 +0900

    メインコンポーネントの命名を変更

---
+++ src/components/MentorHubApp.tsx
@@ -0,0 +1,73 @@
+import { useState } from "react";
+import { UserList } from "./users/UserList";
+import { StudentList } from "./users/StudentList";
+import { MentorList } from "./users/MentorList";
+import { Button } from "./common/Button";
+import { Modal } from "./common/Modal";
+
+// トグルの種類を定義
+type ToggleString = "allUsers" | "students" | "mentors";
+
+export const MentorHubApp = () => {
+  // トグルの初期状態を定義
+  const [activeToggle, setActiveToggle] = useState<ToggleString>("allUsers");
+
+  // ユーザー新規登録ボタンの状態を定義
+  const [isPushed, setIsPushed] = useState<boolean>(false);
+
+  return (
+    <div className="container items-center" role="group">
+      <div className="d-flex justify-content-between m-3">
+        <div
+          className="btn-group"
+          role="group"
+          aria-label="this is radio toggle button group"
+        >
+          <input
+            type="radio"
+            className="btn-check"
+            name="radio-button"
+            id="rb1"
+            checked={activeToggle === "allUsers"}
+            onChange={() => setActiveToggle("allUsers")}
+          />
+          <label className="btn btn-outline-primary" htmlFor="rb1">
+            全ユーザー
+          </label>
+          <input
+            type="radio"
+            className="btn-check"
+            name="radio-button"
+            id="rb2"
+            checked={activeToggle === "students"}
+            onChange={() => setActiveToggle("students")}
+          />
+          <label className="btn btn-outline-primary" htmlFor="rb2">
+            生徒
+          </label>
+          <input
+            type="radio"
+            className="btn-check"
+            name="radio-button"
+            id="rb3"
+            checked={activeToggle === "mentors"}
+            onChange={() => setActiveToggle("mentors")}
+          />
+          <label className="btn btn-outline-primary" htmlFor="rb3">
+            メンター
+          </label>
+        </div>
+        <Button
+          buttonTitle="+ 新規追加"
+          onClick={setIsPushed}
+          value={!isPushed}
+        ></Button>
+      </div>
+      {activeToggle === "allUsers" && <UserList />}
+      {activeToggle === "students" && <StudentList />}
+      {activeToggle === "mentors" && <MentorList />}
+      {/* Todo: 新規登録ボタンを押したらユーザー登録モーダルを表示 */}
+      {isPushed && <Modal />}
+    </div>
+  );
+};
