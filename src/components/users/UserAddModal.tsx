d763e6023edf96b63a9b971e388588945cf7b5b4
lokusuke <suzloku63@gmail.com>
2026-08-12 16:41:30 +0900

    新規登録モーダルにて、趣味や使用言語を選択制に変更

--- src/components/users/UserAddModal.tsx
+++ src/components/users/UserAddModal.tsx
@@ -1,50 +1,85 @@
 import { useForm } from "react-hook-form";
 import { Modal } from "../common/Modal";
 import { Button } from "../common/Button";
-import { appendUser, type NewUserFormat } from "../../store/atoms/userListAtom";
+import { appendUser } from "../../store/atoms/userListAtom";
 import { useSetAtom } from "jotai";
+import {
+  defaultFormValues,
+  type UserForm,
+} from "../../store/data/userFormType";
+import {
+  commonItems,
+  mentorItems,
+  studentItems,
+} from "../../store/data/userRegistrationItemType";
+import { HOBBY_OPTIONS } from "../../store/data/hobbies";
 import type { Mentor, Student } from "../../store/data/userType";
+import { LANGUAGE_OPTIONS } from "../../store/data/languages";
 
-type RegistrationItem<T extends NewUserFormat> = {
-  name: string; // 登録項目名
-  key: keyof T; // register()関数で登録するキー名
+type Props = {
+  isPushed: boolean;
+  setIsPushed: React.Dispatch<React.SetStateAction<boolean>>;
 };
 
-const commonItems: RegistrationItem<NewUserFormat>[] = [
-  { name: "名前", key: "name" },
-  { name: "メールアドレス", key: "email" },
-  { name: "年齢", key: "age" },
-  { name: "住所", key: "postCode" },
-  { name: "電話番号", key: "phone" },
-  { name: "趣味", key: "hobbies" },
-  { name: "Webサイト", key: "url" },
-  { name: "役割", key: "role" },
-];
+export const UserAddModal = ({ isPushed, setIsPushed }: Props) => {
+  const { register, handleSubmit, watch } = useForm<UserForm>({
+    // アンマウントされたコンポーネントの登録内容を削除
+    shouldUnregister: true,
+    // デフォルト値
+    defaultValues: defaultFormValues,
+  });
 
-const studentItems: RegistrationItem<Omit<Student, "id">>[] = [
-  { name: "勉強時間", key: "studyMinutes" },
-  { name: "課題コード", key: "taskCode" },
-  { name: "学習言語", key: "studyLangs" },
-  { name: "スコア", key: "score" },
-];
-
-const mentorItems: RegistrationItem<Omit<Mentor, "id">>[] = [
-  { name: "経験日数", key: "experienceDays" },
-  { name: "使用言語", key: "useLangs" },
-  { name: "対応開始コード", key: "availableStartCode" },
-  { name: "対応終了コード", key: "availableEndCode" },
-];
-
-export const UserAddModal = () => {
-  const { register, handleSubmit, watch } = useForm<NewUserFormat>();
   const addUser = useSetAtom(appendUser);
 
   // useFormの戻り値watchは指定プロパティの値を監視
   const watchRole = watch("role");
 
-  const onSubmit = (data: NewUserFormat) => {
-    // Atomをつかってデータ追加
-    addUser(data);
+  const onSubmit = (data: UserForm) => {
+    const commonUser = {
+      name: data.name,
+      email: data.email,
+      age: data.age,
+      postCode: data.postCode,
+      phone: data.phone,
+      hobbies: data.hobbies,
+      url: data.url,
+    };
+
+    if (data.role === "student") {
+      const newStudent: Omit<Student, "id"> = {
+        ...commonUser,
+        role: "student",
+        studyMinutes: data.studyMinutes,
+        taskCode: data.taskCode,
+        studyLangs: data.studyLangs,
+        score: data.score,
+      };
+
+      console.log(newStudent);
+
+      // 関数Atomを使用してデータを保管
+      addUser(newStudent);
+    }
+
+    if (data.role === "mentor") {
+      const newMentor: Omit<Mentor, "id"> = {
+        ...commonUser,
+        role: "mentor",
+        experienceDays: data.experienceDays,
+        useLangs: data.useLangs,
+        availableStartCode: data.availableStartCode,
+        availableEndCode: data.availableEndCode,
+      };
+
+      console.log(newMentor);
+
+      // 関数Atomを使用してデータを保管
+      addUser(newMentor);
+    }
+
+    // 編集モーダルを閉じる
+    setIsPushed(!isPushed);
+    return;
   };
 
   return (
@@ -54,37 +89,140 @@ export const UserAddModal = () => {
           ユーザー新規登録
         </p>
         <form onSubmit={handleSubmit(onSubmit)}>
-          {commonItems.map((item) =>
-            item.key === "role" ? (
+          {commonItems.map((item) => {
+            if (item.key === "role") {
+              return (
+                <div key={item.key}>
+                  <label>{item.name}</label>
+                  <select
+                    className="border"
+                    {...register(item.key, {
+                      required: `${item.name}は必須です。`,
+                    })}
+                  >
+                    <option value="">{`${item.name}を選択してください`}</option>
+                    <option value="student">生徒</option>
+                    <option value="mentor">メンター</option>
+                  </select>
+                </div>
+              );
+            }
+
+            if (item.key === "hobbies") {
+              return (
+                <div key={item.key}>
+                  <label>{item.name}</label>
+                  {Object.entries(HOBBY_OPTIONS).map(
+                    ([categoryKey, category]) => (
+                      <div key={categoryKey}>
+                        <label>{category.label}</label>
+                        {category.items.map((i) => (
+                          <label key={i}>
+                            <input
+                              type="checkbox"
+                              value={i}
+                              {...register(`${item.key}`, {
+                                required: `${item.name}は必須です。`,
+                              })}
+                            />
+                            {i}
+                          </label>
+                        ))}
+                      </div>
+                    ),
+                  )}
+                </div>
+              );
+            }
+
+            return (
               <div key={item.key}>
                 <label>{item.name}</label>
-                <select className="border" {...register(item.key)}>
-                  <option value="">役割を選択してください</option>
-                  <option value="student">生徒</option>
-                  <option value="mentor">メンター</option>
-                </select>
+
+                <input
+                  className="border"
+                  {...register(
+                    item.key,
+                    item.key === "age"
+                      ? {
+                          valueAsNumber: true,
+                        }
+                      : undefined,
+                  )}
+                />
               </div>
-            ) : (
-              <div key={item.key}>
-                <label>{item.name}</label>
-                <input className="border" {...register(item.key)} />
-              </div>
-            ),
-          )}
+            );
+          })}
           {watchRole === "student" &&
-            studentItems.map((item) => (
-              <div key={item.key}>
-                <label>{item.name}</label>
-                <input className="border" {...register(item.key)} />
-              </div>
-            ))}
+            studentItems.map((item) => {
+              if (item.key === "studyLangs") {
+                return (
+                  <div key={item.key}>
+                    <label>{item.name}</label>
+                    {Object.entries(LANGUAGE_OPTIONS).map(
+                      ([categoryKey, category]) => (
+                        <div key={categoryKey}>
+                          <label>{category.label}</label>
+                          {category.items.map((i) => (
+                            <label key={i}>
+                              <input
+                                type="checkbox"
+                                value={i}
+                                {...register(`${item.key}`, {
+                                  required: `${item.name}は必須です。`,
+                                })}
+                              />
+                              {i}
+                            </label>
+                          ))}
+                        </div>
+                      ),
+                    )}
+                  </div>
+                );
+              }
+              return (
+                <div key={item.key}>
+                  <label>{item.name}</label>
+                  <input className="border" {...register(item.key)} />
+                </div>
+              );
+            })}
           {watchRole === "mentor" &&
-            mentorItems.map((item) => (
-              <div key={item.key}>
-                <label>{item.name}</label>
-                <input className="border" {...register(item.key)} />
-              </div>
-            ))}
+            mentorItems.map((item) => {
+              if (item.key === "useLangs") {
+                return (
+                  <div key={item.key}>
+                    <label>{item.name}</label>
+                    {Object.entries(LANGUAGE_OPTIONS).map(
+                      ([categoryKey, category]) => (
+                        <div key={categoryKey}>
+                          <label>{category.label}</label>
+                          {category.items.map((i) => (
+                            <label key={i}>
+                              <input
+                                type="checkbox"
+                                value={i}
+                                {...register(`${item.key}`, {
+                                  required: `${item.name}は必須です。`,
+                                })}
+                              />
+                              {i}
+                            </label>
+                          ))}
+                        </div>
+                      ),
+                    )}
+                  </div>
+                );
+              }
+              return (
+                <div key={item.key}>
+                  <label>{item.name}</label>
+                  <input className="border" {...register(item.key)} />
+                </div>
+              );
+            })}
 
           <Button buttonTitle="送信" type="submit"></Button>
         </form>
