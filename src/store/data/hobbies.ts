ac8da3da01de8c7114399f2d693bad0281eabeef
lokusuke <suzloku63@gmail.com>
2026-08-14 20:53:26 +0900

    趣味のデータ型を変更

--- src/store/data/hobbies.ts
+++ src/store/data/hobbies.ts
@@ -1,40 +1,19 @@
-export type Sports =
-  | "ランニング"
-  | "筋トレ"
-  | "サッカー"
-  | "野球"
-  | "サーフィン";
-export type Indoor = "ゲーム" | "アニメ" | "映画" | "読書";
-export type Outdoor = "旅行" | "キャンプ" | "登山" | "釣り" | "食べ歩き";
-
-export type Hobbies = {
-  sports: {
-    label: "スポーツ系";
-    items: Sports[];
-  };
-  indoor: {
-    label: "インドア系";
-    items: Indoor[];
-  };
-  outdoor: {
-    label: "アウトドア系";
-    items: Outdoor[];
-  };
-};
-
-export const HOBBY_OPTIONS: Hobbies = {
-  sports: {
+export const HOBBY_OPTIONS = [
+  {
+    id: 1,
     label: "スポーツ系",
     items: ["ランニング", "筋トレ", "サッカー", "野球", "サーフィン"],
   },
-
-  indoor: {
+  {
+    id: 2,
     label: "インドア系",
     items: ["ゲーム", "アニメ", "映画", "読書"],
   },
-
-  outdoor: {
+  {
+    id: 3,
     label: "アウトドア系",
     items: ["旅行", "キャンプ", "登山", "釣り", "食べ歩き"],
   },
-} as const;
+];
+
+export type Sports = typeof HOBBY_OPTIONS;
